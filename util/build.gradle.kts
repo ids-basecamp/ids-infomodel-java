c4e1efb73160a76fb5756137ae5260a46624405d
Glaucio Jannotti <glaucio.jannotti@dengun.com>
2023-12-22 16:38:14 -0300

    feat: refactoring to gradle build

---
+++ util/build.gradle.kts
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       truzzt GmbH - Initial implementation
+ *
+ */
+
+plugins {
+    `java-library`
+}
+
+val jacksonVersion: String by project
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-annotations:${jacksonVersion}")
+}
+
